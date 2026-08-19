0c6514f89eebd27ffc1a295862e50a520e003932
veliyener <velirsg9@gmail.com>
2026-08-19 14:28:41 +0300

    serializer'daki otomatik tekillik doğrulaması kapatıldı

--- companies/serializers.py
+++ companies/serializers.py
@@ -3,6 +3,8 @@ from .models import Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    tax_number = serializers.CharField(max_length=11, validators=[])
+
     class Meta:
         model = Company
         fields = ['id', 'title', 'tax_number', 'status', 'created_at', 'updated_at']
