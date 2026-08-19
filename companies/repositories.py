e0aafee0b1f050eb23f0cfe9c60efbbf339f3fdd
veliyener <velirsg9@gmail.com>
2026-08-19 14:28:08 +0300

    repository katmanına vergi numarası kontrolü eklendi

--- companies/repositories.py
+++ companies/repositories.py
@@ -5,5 +5,8 @@ class CompanyRepository:
     def get_all(self):
         return Company.objects.all()
 
-    def create(self, title, tax_number):
+    def exists_by_tax_number(self, tax_number: str) -> bool:
+        return Company.objects.filter(tax_number=tax_number).exists()
+
+    def create(self, title: str, tax_number: str):
         return Company.objects.create(title=title, tax_number=tax_number)
